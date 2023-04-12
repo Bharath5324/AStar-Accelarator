4b0b57172c9d746dd0317e0a33f4c30b8561c3c0
Bharath5324 <bharath.s@ieee.org>
2023-04-12 13:25:21 +0530

    initial commit

---
+++ memory_manager/memory_manager.srcs/sources_1/new/blockram.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Bharath Srinivasan
+// 
+// Create Date: 04/07/2023 02:00:58 PM
+// Design Name: Blockram - width 35 bit, Depth: 65536
+// Module Name: blockram
+// Project Name: Memory Manager
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module blockram(
+    input clka,
+    input clkb,
+    input ena,
+    input enb,
+    input wea,
+    input web,
+    input [15:0] addra,
+    input [15:0] addrb,
+    input [34:0] dia,
+    input [34:0] dib,
+    output reg [34:0] doa,
+    output reg [34:0] dob
+    );
+    reg [34:0] ram [65535:0];
+    always @(posedge clka)
+    begin
+        if (ena)
+        begin
+            if (wea)
+                ram[addra] <= dia;
+            doa <= ram[addra];
+        end
+    end
+    always @(posedge clkb)
+    begin
+        if (enb)
+        begin
+            if (web)
+                ram[addrb] <= dib;
+            dob <= ram[addrb];
+        end
+    end
+endmodule
---
+++ memory_manager/memory_manager.srcs/sources_1/new/control_path.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/07/2023 02:05:19 PM
+// Design Name: 
+// Module Name: control_path
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control_path(
+    input clk,
+    input enable,
+    output ready
+    );
+endmodule
