25b44ce5dfd34a446797be450f2561275f5cd45b
Bharath5324 <bharath.s@ieee.org>
2023-05-21 19:19:48 +0530

    Update queue

--- priority_queue/priority_queue.srcs/sources_1/new/queue.v
+++ priority_queue/priority_queue.srcs/sources_1/new/queue.v
@@ -20,26 +20,26 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module queue(
-    output reg [15:0] dout,
-    input [15:0] din,
+module queue #(parameter data_width = 83)(
+    output reg [data_width-1:0] dout,
+    input [data_width-1:0] din,
     input en,
     input wr_en,
     input clk
     );
-    parameter reg_depth = 2000 ;
-    reg [15:0] reg_bank [reg_depth:0];
+    parameter reg_depth = 100 ;
+    reg [data_width-1:0] reg_bank [reg_depth:0];
     integer i;
     always @(posedge clk) begin
         if(en && wr_en) begin
             
-            if(din <= reg_bank[0] || reg_bank[0] === 16'hxxxx) begin
+            if(din <= reg_bank[0] || reg_bank[0] === 83'bxxxx) begin
                 $display("true");
                 reg_bank[0] <= din;
             end
             for(i = 1; i<=reg_depth; i = i+1) begin
-                if(din <= reg_bank[i] || reg_bank[i] === 16'hxxxx) begin
-                    if(din <= reg_bank[i-1] || reg_bank[i-1] === 16'hxxxx)
+                if(din[82:51] <= reg_bank[i][82:51] || reg_bank[i][82:51] === 32'bxxxx) begin
+                    if(din[82:51] <= reg_bank[i-1][82:51] || reg_bank[i-1][82:51] === 32'bxxxx)
                         reg_bank[i] <= reg_bank[i-1];
                     else
                         reg_bank[i] <= din;
@@ -47,11 +47,11 @@ module queue(
             end
         end
         else if( en && !wr_en) begin
-            dout  <= reg_bank[0];
             for(i = 0; i<reg_depth; i = i+1)
                 reg_bank[i] <= reg_bank[i+1];
-            reg_bank[reg_depth] <= 16'hxxxx;
+            reg_bank[reg_depth] <= 83'bxxxx;
         end
+       dout  <= reg_bank[0]; 
     end
     
 endmodule
